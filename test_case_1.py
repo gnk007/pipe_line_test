b6821eac968b08b5dfc657ac7872cfc7083274ef
gnk007 <79280871+gnk007@users.noreply.github.com>
2021-03-02 21:05:46 +0100

    Update test_case_1.py

--- test_case_1.py
+++ test_case_1.py
@@ -11,14 +11,17 @@ class Test(unittest.TestCase):
         port = 3333  # The same port as used by the server
         self.s.connect((host, port))
         self.s.send(b'put key=2\0')
-
+        data = self.s.recv(1024)
+        data_str_1 = str(data)
+        self.assertEqual('OK',data_str_1[4:5], "OKE")
+        
     def test_get(self):
-        self.s.send(b"count\0")
+        self.s.send(b'count\0')
         data = self.s.recv(1024)
         print('Received', data)
         data_str = str(data)
         print('Received_str', data_str)
-        self.assertEqual("1",data_str[4], "OKE")
+        self.assertEqual('1',data_str[4], "OKE")
 
     def tearDown(self) -> None:
         self.s.close()
